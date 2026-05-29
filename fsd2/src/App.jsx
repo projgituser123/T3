4819ae9f1a49d70ac3ee3e131c68dd467a42fa81
projgituser123 <codendebug7@gmail.com>
2026-05-29 13:27:24 +0530

    Add files via upload

---
+++ fsd2/src/App.jsx
@@ -0,0 +1,42 @@
+import Comp from "./components/Comp.jsx"
+import Form from "./components/Form.jsx"
+import Event from "./components/Event.jsx"
+import Event2 from "./components/Event2.jsx"
+import Pr1 from "./components/Pr1.jsx"
+import Prod1 from "./components/Prod1"
+import Rule from "./components/Rule.jsx"
+import Map1 from "./components/Map1.jsx"
+import Data_Task1 from "./components/Data_Task1.jsx"
+import Task2 from "./components/task2/Task2.jsx"
+import Main from "./components/task3/Main.jsx"
+import US1 from "./components/US1.jsx"
+import US2 from "./components/US2.jsx"
+import US3 from "./components/US3.jsx"
+import Task from "./components/task4/Task.jsx"
+import Task5 from  "./components/task5/Task5.jsx"
+import Todo from "./components/Todo.jsx"
+function App(){
+  return(
+    <div>
+      <Comp/>
+      <Form/>
+      <Event/>
+      <Event2/>
+      <Pr1/>
+      // write a jsx prgramme in ehich you have to print the names of students and univeristy which are passed as props using json...
+      <Prod1/>
+      <Rule/>
+      <Map1/>
+      <Data_Task1/>
+      <Task2/>
+      <Main/>
+      <US1/>
+      <US2/>
+      <US3/>
+      <Task/>
+      <Task5/>
+      <Todo/>
+    </div>
+  )
+}
+export default App
